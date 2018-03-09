47b2c19544ababb39b0b7430c438f918a351733a
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-09 08:23:37 -0500

    Updated from Brython Server: 3/9/2018 8:23:36 AM

--- functionDemo.py
+++ functionDemo.py
@@ -3,4 +3,18 @@
 #fuctionDemo.py - how to write our own functions
 
 def hw():
-    print('Hello, World"
+    print('Hello, World')
+
+def double(thingToDouble):
+    print(thingToDouble *2)
+
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+hw()
+double(12)
+double('w')
+bigger(4,3)
