05b1e5fb627bb6f1e0bcc2134b4c763a3b7ca017
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-19 08:11:02 -0400

    Updated from Brython Server: 3/19/2018 8:11:01 AM

--- returnDemo.py
+++ returnDemo.py
@@ -1,5 +1,10 @@
 #Heather Stafford
 #3/19/18
-#returnDemo.py
+#returnDemo.py - how to use return
 
+from random import randint
 
+def randEven(low,high):
+    print(randint(low,high/2)*2)
+    
+randEven(0,10)
