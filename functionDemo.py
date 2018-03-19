ecd99c06ac253eb57bd67480f7983b55a669fdf0
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-19 08:05:55 -0400

    Updated from Brython Server: 3/19/2018 8:05:54 AM

--- functionDemo.py
+++ functionDemo.py
@@ -17,9 +17,14 @@ def bigger(a,b):
 def slope(x1, y1,x2, y2):
     print((y2-y1)/(x2-x1))
 
+print("The max if 3 and 4 is", max(3,4))
+print("The max if 3 and 4 is", bigger(3,4))
+
+"""
 hw()
 double(12)
 double('w')
 bigger(4,3)
 bigger("Stafford", "Heath")
 slope(1,1,2,2)
+"""
