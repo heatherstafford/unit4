cf67433061ad683eb4dec526e5ffc0a568078001
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-19 08:20:38 -0400

    Updated from Brython Server: 3/19/2018 8:20:38 AM

--- returnDemo.py
+++ returnDemo.py
@@ -5,6 +5,14 @@
 from random import randint
 
 def randEven(low,high):
-    print(randint(low/2,high/2)*2)
+    n = randint(low,high)
+    while n%2 != 0:
+        n=randint(low,high)
+    return(n)
     
-randEven(11,21)
+print("Your even numbers are", randEven(0,100), randEven(0,100), randEven(0,100))
+num4 = randEven(1000,2000)
+if num4<1500:
+    print(num4 , "is pretty big")
+else:
+    print(num4, "is really big")
