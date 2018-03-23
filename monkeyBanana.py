ca42b9064c71d1a617a92f121806d90cfdb95086
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-23 08:49:20 -0400

    Updated from Brython Server: 3/23/2018 8:49:18 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -9,6 +9,7 @@ ROWS = 20
 COLS = 40
 CELL_SIZE = 20
 
+#moves the monkey right 1 cell if possible
 def moveRight(Event):
     if monkey.x < (COLS-1)*(CELL_SIZE):
         monkey.x += CELL_SIZE
@@ -16,6 +17,7 @@ def moveRight(Event):
             moveBanana()
             updateScore()
     
+#moves the monkey left 1 cell if possible
 def moveLeft(Event):
     if monkey.x>0:
         monkey.x -= CELL_SIZE
@@ -23,6 +25,7 @@ def moveLeft(Event):
             moveBanana()
             updateScore()
     
+#moves the monkey up 1 cell if possible
 def moveUp(Event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
@@ -30,6 +33,7 @@ def moveUp(Event):
             moveBanana()
             updateScore()
     
+#moves the monkey down 1 cell if possible
 def moveDown(Event):
     if monkey.y < (ROWS-1)*(CELL_SIZE):
         monkey.y += CELL_SIZE
@@ -37,22 +41,26 @@ def moveDown(Event):
             moveBanana()
             updateScore()
     
+#move banana to a random location
 def moveBanana():
-    data['frames'] = 0
+    data['frames'] = 0 #reset the timer
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
     
+#increase score and display new text at bottom of screen
 def updateScore():
     data['score'] += 10
-    data['scoreText'].destroy()
+    data['scoreText'].destroy() #remove old writing
     scoreBox = TextAsset('Score = ' + str(data['score']))
     data['scoreText'] = Sprite(scoreBox,(0,ROWS *CELL_SIZE))
     
+#keeps track of how many frames have happened and moves banana if it has been more than 300 frames since last move
 def step():
     data['frames'] += 1 
     if data['frames']%300 == 0:
         moveBanana()
 
+#sets up and runs the game
 if __name__ == '__main__':
     
     #hold variables in a dictionary
@@ -60,10 +68,12 @@ if __name__ == '__main__':
     data['score'] = 0
     data['frames'] = 0
     
+    #colors
     green = Color(0x006600,1)
     brown = Color(0x8B4513,1)
     yellow = Color(0xFFFF00,1)
     
+    #graphics
     jungleBox = RectangleAsset(CELL_SIZE * COLS, CELL_SIZE*ROWS, LineStyle(1,green), green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown), brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown), yellow)
