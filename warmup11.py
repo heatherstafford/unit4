25f3b21ebbf27b10373ad418a2c6439425588ed2
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-26 08:28:15 -0400

    Updated from Brython Server: 3/26/2018 8:28:15 AM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,15 @@
+#Heather Stafford
+#3/26/18
+#warmup11
 
+
+def prime(n):
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    return True
+
+print(prime(9))
+print(prime(10))
+print(prime(11))
+    
