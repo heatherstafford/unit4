367d4e0c8af4c0615c98e5fd89d7daf771050552
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-29 08:44:42 -0400

    Updated from Brython Server: 3/29/2018 8:44:42 AM

--- stringUnion.py
+++ stringUnion.py
@@ -3,10 +3,10 @@
 #stringUnion.py
 
 def stringunion(word1,word2):
+    answer = ''
     for ch in word1 + word2:
-        print(ch)
-    answer = (' ')
-    if ch not in answer:
-        answer = answer + ch
+        if ch not in answer:
+            answer = answer + ch
+    return(answer)
     
 print(stringunion('mississippi', 'pennsylvania'))
