6137088b5e589426689101831ab25f39d35d5828
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-29 08:30:49 -0400

    Updated from Brython Server: 3/29/2018 8:30:49 AM

--- stringUnion.py
+++ stringUnion.py
@@ -1,3 +1,9 @@
 #Heather Stafford
 #3/29/19
 #stringUnion.py
+
+def stringunion(word1,word2):
+    char1 = len(word1)
+    char2 = len(word2)
+    
+print(stringunion('mississippi', 'pennsylvania'))
