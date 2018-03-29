c1cfa7cf1d6f657abc8034b9bd2c66a28d1def99
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-29 08:19:15 -0400

    Updated from Brython Server: 3/29/2018 8:19:15 AM

--- warmup12.py
+++ warmup12.py
@@ -3,8 +3,8 @@
 #warmup12.py - returns the GCF of two numbers
 
 def gcf(x,y):
-    for i in range(2,x):
+    for i in range(x,0,-1):
         if x%i == 0 and y%i == 0:
             return(i)
 
-print(gcf(12,15))
+print(gcf(5,9))
