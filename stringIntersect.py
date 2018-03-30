1293c641f799c071f3e964fcb7bf0abd9c1fc8e5
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-30 08:23:56 -0400

    Updated from Brython Server: 3/30/2018 8:23:55 AM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,13 @@
+#Heather Stafford
+#3/30/18
+#stringIntersect.py
 
+def stringIntersect(word1,word2):
+    answer = word2
+    for ch in word 1:
+        if ch in answer:
+            answer = answer - ch
+    return(answer)
+
+stringIntersect(mississippi, pensylvania)
+        
