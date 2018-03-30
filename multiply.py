10cb6e8d35795fe1e803708a32aa9581071b86c4
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-03-30 08:49:20 -0400

    Updated from Brython Server: 3/30/2018 8:49:19 AM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,18 @@
+#Heather Stafford
+#3/30/18
+#multiply.py
 
+from random import randint
+
+
+numCorrect = 0
+while numCorrect < 5:
+    num1 = randint(1,12)
+    num2 = randint(1,12)
+    answer = int(input(num1 ,'x', num2, '=' ))
+    if answer == num1 * num2:
+        numCorrect += 1
+    else:
+        print('Sorry, Incorrect')
+    
+        
