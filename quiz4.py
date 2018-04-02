78332c0866e14624bb39409191e78011c3213a4f
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-04-02 08:26:10 -0400

    Updated from Brython Server: 4/2/2018 8:26:09 AM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,28 @@
+#Heather Stafford
+#4/2/18
+#quiz4.py
 
+def count(x):
+    for i in range(1,x + 1):
+        print(i)
+        
+def excitedPrint(word1):
+    print(word1.upper() + '!!!')
+    
+def firstLetter(word2): 
+    letter = ''
+    for ch in word2:
+        if ch not in letter:
+            letter = letter + ch
+        return(letter)
+
+def repeats(a,b,c):
+    if a == b or a == c or b ==c:
+        return(True)
+    else:
+        return(False)
+
+count(7)
+excitedPrint('I <3 programming')
+print(firstLetter('Stafford'))
+print(repeats(5,6,5))
